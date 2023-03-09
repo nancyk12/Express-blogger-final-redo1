36e9a4b28c4ca3efe551e5a8c7501830484f8e6d
nancyk12 <nancyk12@hotmail.com>
2023-03-09 15:06:47 -0500

    router for controllers/taskListControllers.js

--- routes/taskList.js
+++ routes/taskList.js
@@ -2,4 +2,28 @@ const {v4: uuidv4} = require("uuid");
 const express = require("express");
 const router = express.Router();
 
+// Required Functionality (Routes):
+const listController =require('../controllers/taskListController')
+
+
+//1. Create Task
+router.post('/create-task', listController.createTask);
+
+//2. Update Task (Mark as Completed/ Uncompleted)
+router.put('/update-task', listController.updateTask);
+
+//3. Delete Task
+router.delete('/delete-task',listController.deleteTask);
+
+//4. Delete Multipl Tasks
+router.delete('/delete-multi', listController.deleteMultiple);
+
+//5. Create Muliple Tasks
+router.post('./create-multi', listController.createMultiple);
+
+//6. Display all Tasks
+router.get('/all,' listController.getAllTasks);
+
+
+
 module.exports = router;
